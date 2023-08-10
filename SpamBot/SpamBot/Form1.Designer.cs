2d0ee11698de2400001de745d6d6e11b3cd0890a
Thato Omphile Nameng <71035775+Thato-Nameng@users.noreply.github.com>
2023-08-10 23:10:29 +0200

    Add files via upload

---
+++ SpamBot/SpamBot/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SpamBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lblSpam = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnStart.Font = new System.Drawing.Font("Perpetua Titling MT", 15F);
+            this.btnStart.Location = new System.Drawing.Point(3, 2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(210, 66);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnStop.Font = new System.Drawing.Font("Perpetua Titling MT", 15F);
+            this.btnStop.Location = new System.Drawing.Point(214, 2);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(210, 66);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // lblSpam
+            // 
+            this.lblSpam.AutoSize = true;
+            this.lblSpam.Font = new System.Drawing.Font("Perpetua Titling MT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSpam.Location = new System.Drawing.Point(154, 71);
+            this.lblSpam.Name = "lblSpam";
+            this.lblSpam.Size = new System.Drawing.Size(0, 23);
+            this.lblSpam.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(426, 99);
+            this.Controls.Add(this.lblSpam);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Spam";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label lblSpam;
+    }
+}
+
---
+++ SpamBot/SpamBot/Spam.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpamBot
+{
+    public partial class Form1 : Form
+    {
+        private List<Process> notepadProcesses = new List<Process>();
+        private string spamMessage = "Yes you are getting spammed\n";
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            IntPtr foregroundWindow = GetForegroundWindow();
+            GetWindowThreadProcessId(foregroundWindow, out int processId);
+
+            foreach (Process process in notepadProcesses)
+            {
+                if (process.Id == processId)
+                {
+                    IntPtr hwnd = process.MainWindowHandle;
+                    SendKeys.SendWait(spamMessage);
+                    SendKeys.SendWait("{Enter}");
+                    Thread.Sleep(50); 
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++) 
+            {
+                Process notepadProcess = Process.Start("notepad.exe");
+                if (notepadProcess != null)
+                {
+                    notepadProcesses.Add(notepadProcess);
+                }
+            }
+            timer.Start();
+            lblSpam.Text = "Spamming..."; 
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            foreach (Process process in notepadProcesses)
+            {
+                process.CloseMainWindow(); 
+            }
+            notepadProcesses.Clear();
+            timer.Stop();
+            lblSpam.Text = "Spamming stopped.";
+            MessageBox.Show("The SPAM has stopped.");
+        }
+    }
+}
